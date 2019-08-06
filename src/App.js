e03a73e8744eb1c118bc68d6658b917143c843ce
Karina Gaulin <karina@domandtom.com>
2019-08-06 11:21:28 -0400

    Add state and methods, make api call

--- src/App.js
+++ src/App.js
@@ -2,16 +2,70 @@ import React from 'react';
 import './App.css';
 import Title from './Title';
 import SearchAndFilter from './SearchAndFilter';
-import BookList from './BookList';
+// import BookList from './BookList';
 
-function App() {
-  return (
-    <div className="app">
-      <Title />
-      <SearchAndFilter />
-      <BookList />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      // printType: 'All'
+    };
+  }
+  // handleChange = (event) => {
+  //   this.setState({ printType: event.target.value })
+  // }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    
+    console.log(`Search called ${e}`);
+    
+  }
+
+  componentDidMount() {
+    const url =`https://www.googleapis.com/books/v1/volumes?q=henry`;
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "BearerAIzaSyBh6F-ZE_Y49xI4HZ6D4KYw0ZkIWaioN24",
+        "Content Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(results => {
+        if(!results.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return results;
+      })
+      .then(results => results.json())
+      .then(data => {
+        this.setState({
+          books: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <SearchAndFilter 
+          handleSearch={this.handleSearch}
+          handleChange={this.handleChange}/>
+        {/* <BookList /> */}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/SearchAndFilter.js
+++ src/SearchAndFilter.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import './SearchAndFilter.css';
 
-function Search() {
-  return (
-    <div className="search">
-      <form className="search-books">
-        <label htmlFor="search-books ">Search:</label>
-        <input 
-          type="text"
-          name="search-books"
-          id="search-books"
-          placeholder="henry"
-          required/>
-
-        <button type="submit">Search</button>
-      </form>
-      <form>
-        <label htmlFor="print-type">Print 
+class SearchAndFilter extends React.Component {
+  render() {    
+    return (
+      <div className="search">
+        <form className="search-books" onSubmit={e => this.props.handleSearch(e)} >
+          <label htmlFor="search-books">Search:</label>
+          <input
+            type="text"
+            name="search-books"
+            id="search-books"
+            placeholder="henry"
+            required />
+          
+          <button type="submit">Search</button>
+        </form>
+        {/* <form type="filter" >
+          <label htmlFor="print-type">Print
         Type: </label>
-        <select id="print-type" name="print-type">
-          <option value="All">Select one...</option>
-          Options
-        </select>
-      </form>
-    </div>
-  );
+          <select 
+            value={this.state.printType}
+            onChange={this.props.handleChange}
+            id="print-type" 
+            name="print-type">
+              <option value="All">All</option>
+              <option value="Books">Books</option>
+              <option value="Magazines">Magazines</option>
+          </select>
+        </form> */}
+      </div>
+    );
+  }
+  
 }
 
-export default Search;
+export default SearchAndFilter;
